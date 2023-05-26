e7eac2a6eb38555ee8deaf0fb4c89c36f4204657
Kamil Wenta <kamil.wenta222@gmail.com>
2023-05-26 21:01:09 +0200

    Fix nullable fields in courses table

--- db/schema.ts
+++ db/schema.ts
@@ -2,7 +2,15 @@ import { mysqlEnum, mysqlTable, serial, varchar } from "drizzle-orm/mysql-core"
 
 export const courses = mysqlTable("courses", {
   id: serial("id").primaryKey(),
-  name: varchar("name", { length: 256 }),
-  type: mysqlEnum("type", ["lecture", "tutorial", "laboratory", "project"]),
-  frequency: mysqlEnum("frequency", ["every_week", "every_two_weeks"]),
+  name: varchar("name", { length: 256 }).notNull(),
+  type: mysqlEnum("type", [
+    "lecture",
+    "tutorial",
+    "laboratory",
+    "project",
+  ]).notNull(),
+  frequency: mysqlEnum("frequency", [
+    "every_week",
+    "every_two_weeks",
+  ]).notNull(),
 })
