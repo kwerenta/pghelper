022c00ba0da928e2ddd469ca4dc6a30092b7c92b
Kamil Wenta <kamil.wenta222@gmail.com>
2023-05-26 09:44:24 +0200

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "PGHelper",
+  description: "Tools for PG students.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+}
