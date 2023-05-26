3dc1940fe4eff42a72edc79b1ea120a7620dfbc1
Kamil Wenta <kamil.wenta222@gmail.com>
2023-05-26 22:49:33 +0200

    Upgrade typescript to 5.0.4

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Timetable } from "@/components/timetable"
 
-export default function IndexPage() {
+export default async function IndexPage() {
+  // Workaround for Typescript server component cannot be used as a JSX component error
+  const timetable: JSX.Element = await Timetable()
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <Timetable />
+      {timetable}
     </section>
   )
 }
