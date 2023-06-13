cea1e9164035e6e198708ad69b7356a182127441
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-13 23:07:52 +0200

    Fix auth layout function name

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ interface LayoutProps {
   children: React.ReactNode
 }
 
-export default function RootLayout({ children }: LayoutProps) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <div className="container grid min-h-screen items-center">{children}</div>
   )
