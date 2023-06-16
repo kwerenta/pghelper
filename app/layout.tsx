9cf8396d64a698027ea983c77ffd6a8d2c004d9a
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-16 21:42:47 +0200

    Unify layout and page declartions

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,11 @@ export const metadata: Metadata = {
   },
 }
 
-interface RootLayoutProps {
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode
-}
-
-export default function RootLayout({ children }: RootLayoutProps) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
