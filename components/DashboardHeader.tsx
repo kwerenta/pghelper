bf8c5f733b0cb79dd73e10a25496ee0ca911124a
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-26 20:10:37 +0200

    Refactor dashboard pages layout

---
+++ components/DashboardHeader.tsx
@@ -0,0 +1,28 @@
+import type { ReactNode } from "react"
+
+import { Separator } from "@/components/ui/Separator"
+
+interface DashboardHeaderProps {
+  children?: ReactNode
+  title: string
+  description: string
+}
+
+export function DashboardHeader({
+  children,
+  title,
+  description,
+}: DashboardHeaderProps) {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">{title}</h2>
+          <p className="text-muted-foreground">{description}</p>
+        </div>
+        {children}
+      </div>
+      <Separator className="my-6" />
+    </>
+  )
+}
