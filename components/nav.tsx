211386ab1779e88db13514b69d4f8d5f9ebbf1e9
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-27 14:27:14 +0200

    Refactor sidebar structure

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,13 +3,9 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { NavItem } from "@/types"
-import { signOut } from "next-auth/react"
 
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/Icons"
-
-import { Button } from "./ui/Button"
-import { Separator } from "./ui/Separator"
 
 interface SidebarNavProps {
   items: NavItem[]
@@ -23,47 +19,38 @@ export function SidebarNav({ items }: SidebarNavProps) {
   }
 
   return (
-    <nav className="flex h-full flex-col justify-start gap-2 py-6 lg:py-10">
+    <div className="flex-1">
       <Link href="/">
-        <span className="mb-4 flex items-center text-2xl font-bold tracking-tight lg:mb-10">
+        <span className="mb-6 flex items-center text-2xl font-bold tracking-tight lg:mb-10">
           <Icons.graduationCap className="mr-2 h-6 w-6" />
           <span>PGHelper</span>
         </span>
       </Link>
-      {items.map((item, index) => {
-        const Icon = Icons[item.icon || "logo"]
-        return (
-          item.href && (
-            <Link
-              key={index}
-              href={item.disabled ? "/" : item.href}
-              target={item.external ? "_blank" : "_self"}
-            >
-              <span
-                className={cn(
-                  "flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
-                  path === item.href ? "bg-accent" : "transparent",
-                  item.disabled && "cursor-not-allowed opacity-80"
-                )}
+      <nav className="flex h-full flex-col justify-start gap-2">
+        {items.map((item, index) => {
+          const Icon = Icons[item.icon || "logo"]
+          return (
+            item.href && (
+              <Link
+                key={index}
+                href={item.disabled ? "/" : item.href}
+                target={item.external ? "_blank" : "_self"}
               >
-                <Icon className="mr-2 h-4 w-4" />
-                <span>{item.title}</span>
-              </span>
-            </Link>
+                <span
+                  className={cn(
+                    "flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
+                    path === item.href ? "bg-accent" : "transparent",
+                    item.disabled && "cursor-not-allowed opacity-80"
+                  )}
+                >
+                  <Icon className="mr-2 h-4 w-4" />
+                  <span>{item.title}</span>
+                </span>
+              </Link>
+            )
           )
-        )
-      })}
-      <div className="mt-auto">
-        <Separator className="my-4" />
-        <Button
-          variant="ghost"
-          className="w-full justify-start"
-          onClick={() => signOut()}
-        >
-          <Icons.logOut className="mr-2 h-4 w-4" />
-          <span>Log out</span>
-        </Button>
-      </div>
-    </nav>
+        })}
+      </nav>
+    </div>
   )
 }
