63d8dbcdf1b1bd4c9d6f91dd64bfbf04618d0b45
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-27 14:27:14 +0200

    Refactor sidebar structure

--- components/userButton.tsx
+++ components/userButton.tsx
@@ -2,10 +2,18 @@
 
 import { signOut } from "next-auth/react"
 
+import { Icons } from "./Icons"
 import { Button } from "./ui/Button"
 
-export const UserButton = () => (
-  <Button variant="ghost" onClick={() => signOut()}>
-    Log out
-  </Button>
-)
+export function UserButton() {
+  return (
+    <Button
+      variant="ghost"
+      className="w-full justify-start"
+      onClick={() => signOut()}
+    >
+      <Icons.logOut className="mr-2 h-4 w-4" />
+      <span>Log out</span>
+    </Button>
+  )
+}
