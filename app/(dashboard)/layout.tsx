6dd2e0c69942ac9580f76d77dcb36d6c18fd0f50
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-27 14:57:40 +0200

    Add user dropdown menu to sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { siteConfig } from "@/config/site"
+import { getCurrentUser } from "@/lib/session"
 import { Separator } from "@/components/ui/Separator"
 import { SidebarNav } from "@/components/Nav"
 import { UserButton } from "@/components/UserButton"
@@ -8,13 +9,17 @@ export default async function Layout({
 }: {
   children: React.ReactNode
 }) {
+  const user = await getCurrentUser()
+
+  if (!user) return null
+
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <div className="container grid flex-1 lg:grid-cols-[200px_1fr] lg:gap-12">
         <aside className="flex flex-col py-6 lg:sticky lg:top-0 lg:h-screen lg:w-[200px] lg:py-10">
           <SidebarNav items={siteConfig.sidebarNav} />
           <Separator className="my-4" />
-          <UserButton />
+          <UserButton user={user} />
         </aside>
         <main className="flex w-full flex-1 flex-col overflow-hidden">
           {children}
--- components/userButton.tsx
+++ components/userButton.tsx
@@ -1,19 +1,57 @@
 "use client"
 
+import Link from "next/link"
+import { User } from "next-auth"
 import { signOut } from "next-auth/react"
 
 import { Icons } from "./Icons"
 import { Button } from "./ui/Button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/DropdownMenu"
 
-export function UserButton() {
+export function UserButton({ user }: { user: User }) {
   return (
-    <Button
-      variant="ghost"
-      className="w-full justify-start"
-      onClick={() => signOut()}
-    >
-      <Icons.logOut className="mr-2 h-4 w-4" />
-      <span>Log out</span>
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="w-full justify-start">
+          <Icons.user className="mr-2 h-4 w-4" />
+          <span className="truncate">{user.name || user.email || "User"}</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-muted-foreground">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/settings">
+            <Icons.settings className="mr-2 h-4 w-4" />
+            <span className="truncate">Settings</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onSelect={(e) => {
+            e.preventDefault()
+            signOut()
+          }}
+        >
+          <Icons.logOut className="mr-2 h-4 w-4" />
+          <span className="truncate">Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
