c00692e125920275fefce46afd8230d95e8b5881
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-28 18:54:43 +0200

    Fine-tune timetable design

--- components/Timeslot.tsx
+++ components/Timeslot.tsx
@@ -2,7 +2,7 @@ import { Course } from "@/db/schema"
 
 export function Timeslot({ course }: { course?: Course }) {
   return (
-    <td className="min-w-[16rem] border-l-2 border-l-muted-foreground p-2 capitalize sm:px-3 md:min-w-[12rem] lg:min-w-0 lg:px-4">
+    <td className="min-w-[12rem] rounded-lg bg-muted p-2 capitalize md:p-3">
       {!course ? null : (
         <>
           <span>[{course.type}]</span>
