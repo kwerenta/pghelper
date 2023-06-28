08eed7f394c7f2b70b6f0a0cc84e0f8fab14ef97
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-28 18:54:43 +0200

    Fine-tune timetable design

--- components/Timetable.tsx
+++ components/Timetable.tsx
@@ -6,6 +6,7 @@ import {
 import { getUserTimetable } from "@/lib/timetable"
 
 import { Timeslot } from "./Timeslot"
+import { Card, CardContent } from "./ui/card"
 
 const hoursArray = Array.from(
   { length: LAST_SUBJECT_HOUR - FIRST_SUBJECT_HOUR + 1 },
@@ -13,40 +14,42 @@ const hoursArray = Array.from(
 )
 
 export async function Timetable() {
-  const entries = await getUserTimetable({ id: "", deanGroup: 3 })
+  const entries = await getUserTimetable({ id: "", deanGroup: 5 })
 
   return (
-    <div className="flex overflow-x-auto">
-      <table className="mb-4 flex-1 text-foreground">
-        <thead>
-          <tr>
-            <th className="sticky left-0 bg-background py-2">Hour</th>
-            {DAYS_OF_WEEK.map((day, i) => (
-              <th key={i} className="capitalize">
-                {day}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {hoursArray.map((hour) => (
-            <tr key={hour} className="text-center">
-              <th className="sticky left-0 bg-background p-2 capitalize sm:px-3 lg:px-4">
-                {hour}:00
-              </th>
-              {DAYS_OF_WEEK.map((weekday) => {
-                const course = entries.find(
-                  (entry) =>
-                    entry.weekday === weekday &&
-                    entry.startTime <= hour &&
-                    entry.endTime > hour
-                )?.course
-                return <Timeslot key={weekday + hour} course={course} />
-              })}
+    <Card className="overflow-x-auto">
+      <CardContent className="pl-0">
+        <table className="border-separate border-spacing-2">
+          <thead>
+            <tr>
+              <th className="sticky left-0 bg-background py-2">Hour</th>
+              {DAYS_OF_WEEK.map((day, i) => (
+                <th key={i} className="capitalize">
+                  {day}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {hoursArray.map((hour) => (
+              <tr key={hour} className="text-center">
+                <th className="sticky left-0 bg-background px-2 align-text-top capitalize md:px-3">
+                  {hour}:00
+                </th>
+                {DAYS_OF_WEEK.map((weekday) => {
+                  const course = entries.find(
+                    (entry) =>
+                      entry.weekday === weekday &&
+                      entry.startTime <= hour &&
+                      entry.endTime > hour
+                  )?.course
+                  return <Timeslot key={weekday + hour} course={course} />
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </CardContent>
+    </Card>
   )
 }
