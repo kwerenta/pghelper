d1e2649f11843751ab439d94f1c8f522bde389ce
Kamil Wenta <kamil.wenta222@gmail.com>
2023-06-30 21:37:30 +0200

    Add deanGroup to user profile

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -6,6 +6,7 @@ type UserId = string
 declare module "next-auth/jwt" {
   interface JWT {
     id: UserId
+    deanGroup: number
   }
 }
 
@@ -13,6 +14,7 @@ declare module "next-auth" {
   interface Session {
     user: User & {
       id: UserId
+      deanGroup: number
     }
   }
 }
