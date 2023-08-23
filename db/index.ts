aa4d79d962dafe31b0a2ef185626d3ff4b22bd1b
Kamil Wenta <kamil.wenta222@gmail.com>
2023-08-23 19:41:55 +0200

    Update drizzle-orm to 0.28.3

--- db/index.ts
+++ db/index.ts
@@ -3,6 +3,6 @@ import { drizzle } from "drizzle-orm/mysql2"
 import { createConnection } from "mysql2"
 
 const connection = createConnection(process.env.DATABASE_URL!)
-export const db = drizzle(connection, { schema })
+export const db = drizzle(connection, { schema, mode: "default" })
 
 export type DatabaseInstance = typeof db
