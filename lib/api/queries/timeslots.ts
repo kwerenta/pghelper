a1ecf170b48ee6ae124ab0971c61dce83c34fa0c
Kamil Wenta <kamil.wenta222@gmail.com>
2024-03-30 00:00:32 +0100

    Refactor getUserTimetable query

--- lib/api/queries/timeslots.ts
+++ lib/api/queries/timeslots.ts
@@ -23,40 +23,42 @@ import { getCurrentUser } from "@/lib/session"
 
 export const getUserTimetable = async () => {
   const user = await getCurrentUser()
-
   if (!user) notFound()
 
+  const overridedTimeslots = db.$with("overrided_timeslots").as(
+    db
+      .select({
+        timeslotId: timeslotOverrides.timeslotId,
+        courseId: timeslotOverrides.courseId,
+      })
+      .from(timeslotOverrides)
+      .where(eq(timeslotOverrides.studentId, user.id)),
+  )
+
   const overridedTimeslotIds = db
-    .select({
-      timeslotId: timeslotOverrides.timeslotId,
-    })
-    .from(timeslotOverrides)
-    .where(eq(timeslotOverrides.studentId, user.id))
+    .select({ timeslotId: overridedTimeslots.timeslotId })
+    .from(overridedTimeslots)
 
   const overridedCourseIds = db
-    .select({
-      courseId: timeslotOverrides.courseId,
-    })
-    .from(timeslotOverrides)
-    .where(eq(timeslotOverrides.studentId, user.id))
+    .select({ courseId: overridedTimeslots.courseId })
+    .from(overridedTimeslots)
 
-  return (
-    await db
-      .select()
-      .from(timeslots)
-      .innerJoin(courses, eq(timeslots.courseId, courses.id))
-      .where(
-        or(
-          and(isNull(timeslots.deanGroupId), isNull(timeslots.subgroup)),
-          inArray(timeslots.id, overridedTimeslotIds),
-          and(
-            eq(timeslots.deanGroupId, user.deanGroup.id),
-            isNull(timeslots.subgroup),
-            notInArray(timeslots.courseId, overridedCourseIds),
-          ),
+  return await db
+    .with(overridedTimeslots)
+    .select({ ...getTableColumns(timeslots), course: courses })
+    .from(timeslots)
+    .innerJoin(courses, eq(timeslots.courseId, courses.id))
+    .where(
+      or(
+        and(isNull(timeslots.deanGroupId), isNull(timeslots.subgroup)),
+        inArray(timeslots.id, overridedTimeslotIds),
+        and(
+          eq(timeslots.deanGroupId, user.deanGroup.id),
+          isNull(timeslots.subgroup),
+          notInArray(timeslots.courseId, overridedCourseIds),
         ),
-      )
-  ).map((entry) => ({ ...entry.timeslot, course: entry.course }))
+      ),
+    )
 }
 export type TimetableEntry = Awaited<
   ReturnType<typeof getUserTimetable>
